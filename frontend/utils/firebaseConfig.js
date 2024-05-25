d61e260831f521ef878b2619316ff34be99346e0
Karthikeyan T <113615223+KarthiT24@users.noreply.github.com>
2024-05-25 12:37:52 +0530

    Update firebaseConfig.js

--- frontend/utils/firebaseConfig.js
+++ frontend/utils/firebaseConfig.js
@@ -2,15 +2,15 @@ import { getApps, initializeApp } from '@firebase/app';
 import { getAuth, initializeAuth, getReactNativePersistence } from '@firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBejzfwpu1lyNQg07jaQ-qyo0HqrBImgUo",
-  authDomain: "placementprediction-fp.firebaseapp.com",
-  projectId: "placementprediction-fp",
-  storageBucket: "placementprediction-fp.appspot.com",
-  messagingSenderId: "403450492438",
-  appId: "1:403450492438:web:e307336c971fde21b4a737",
-  measurementId: "G-6W2STCNVCX"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBejzfwpu1lyNQg07jaQ-qyo0HqrBImgUo",
+//   authDomain: "placementprediction-fp.firebaseapp.com",
+//   projectId: "placementprediction-fp",
+//   storageBucket: "placementprediction-fp.appspot.com",
+//   messagingSenderId: "403450492438",
+//   appId: "1:403450492438:web:e307336c971fde21b4a737",
+//   measurementId: "G-6W2STCNVCX"
+// };
 
 let app;
 let auth;
